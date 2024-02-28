0f61d5cdb6651dc53d1dd06674d683e32b741875
TimiSeila <timi.seila@gmail.com>
2024-02-28 15:07:31 +0200

    Parallax effect added to home section

--- src/features/HomeSection/HomeSection.jsx
+++ src/features/HomeSection/HomeSection.jsx
@@ -1,5 +1,7 @@
-import React from "react";
-import { FaChevronDown, FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
+import React, { useEffect } from "react";
+import { FaChevronDown, FaGithub, FaLinkedin } from "react-icons/fa";
+import { IoTriangleOutline, IoSquareOutline } from "react-icons/io5";
+import { MdOutlineCircle } from "react-icons/md";
 import "./HomeSection.css";
 
 const HomeSection = () => {
@@ -7,31 +9,87 @@ const HomeSection = () => {
     window.open(url, "_blank");
   };
 
+  function generateRandomNumber() {
+    const randomNumber = Math.random() * 21 - 10;
+
+    const wholeNumber = Math.round(randomNumber);
+
+    return wholeNumber == 0 ? 1 : wholeNumber;
+  }
+
+  useEffect(() => {
+    const parallax = (e) => {
+      document.querySelectorAll(".parallax-item").forEach((item) => {
+        let moving_value = item.getAttribute("data");
+        let x = (e.clientX * moving_value) / 100;
+        let y = (e.clientY * moving_value) / 100;
+
+        item.style.transform = `translateX(${x}px) translateY(${y}px)`;
+      });
+    };
+
+    document.addEventListener("mousemove", parallax);
+
+    return () => {
+      document.removeEventListener("mousemove", parallax);
+    };
+  }, []);
+
+  const renderImages = () => {
+    const imageArray = Array.from({ length: 28 }, (_, index) => (
+      <React.Fragment key={index}>
+        <IoTriangleOutline
+          data={generateRandomNumber()}
+          className="parallax-item"
+          size={60}
+          color="#000"
+        />
+        <IoSquareOutline
+          data={generateRandomNumber()}
+          className="parallax-item"
+          size={60}
+          color="#ff8f5c"
+        />
+        <MdOutlineCircle
+          data={generateRandomNumber()}
+          className="parallax-item"
+          size={60}
+          color="#34c45a"
+        />
+      </React.Fragment>
+    ));
+
+    return imageArray;
+  };
+
   return (
-    <div className="homesection-container">
-      <div className="homesection-socials-container">
-        <FaGithub
-          className="homesection-socials-icon"
-          onClick={() => redirect("https://github.com/TimiSeila")}
-        />
-        <FaLinkedin
-          className="homesection-socials-icon"
-          onClick={() =>
-            redirect("https://www.linkedin.com/in/timi-seila-aa38bb2a3/")
-          }
-        />
+    <>
+      <div className="homesection-parallax">{renderImages()}</div>
+      <div className="homesection-container">
+        <div className="homesection-socials-container">
+          <FaGithub
+            className="homesection-socials-icon"
+            onClick={() => redirect("https://github.com/TimiSeila")}
+          />
+          <FaLinkedin
+            className="homesection-socials-icon"
+            onClick={() =>
+              redirect("https://www.linkedin.com/in/timi-seila-aa38bb2a3/")
+            }
+          />
+        </div>
+        <div className="homesection-header-container">
+          <p className="homesection-header-name">Timi Seila</p>
+          <p className="homesection-header-desc">Software developer</p>
+        </div>
+        <div className="homesection-about-button-container">
+          <button className="homesection-about-button">About Me</button>
+        </div>
+        <div className="homesection-chevron-container">
+          <FaChevronDown className="homesection-chevron" />
+        </div>
       </div>
-      <div className="homesection-header-container">
-        <p className="homesection-header-name">Timi Seila</p>
-        <p className="homesection-header-desc">Software developer</p>
-      </div>
-      <div className="homesection-about-button-container">
-        <button className="homesection-about-button">About Me</button>
-      </div>
-      <div className="homesection-chevron-container">
-        <FaChevronDown className="homesection-chevron" />
-      </div>
-    </div>
+    </>
   );
 };
 
